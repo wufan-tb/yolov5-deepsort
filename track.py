09836e09d6e6654d29db1d89c57f57bfcf45b406
wufan <1971113508@qq.com>
2020-12-30 08:29:03 +0800

    update

--- track.py
+++ track.py
@@ -184,7 +184,7 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='./weights/yolov5l.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='./inference/video/car.mp4', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--source', type=str, default='D:/dataset/video/car.mp4', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='./inference/output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
